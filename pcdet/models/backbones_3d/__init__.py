fa0290636312a6deeba04fa6e2a906df7a231762
Nguyễn Công Anh Khoa <nbcanhkhoa@gmail.com>
2022-04-23 14:31:24 +0700

    add votr (voxel transformer) to backbone 3d

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -1,6 +1,7 @@
 from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG
 from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x
 from .spconv_unet import UNetV2
+from .votr_backbone import VoxelTransformer, VoxelTransformerV2, VoxelTransformerV3
 
 __all__ = {
     'VoxelBackBone8x': VoxelBackBone8x,
@@ -8,4 +9,6 @@ __all__ = {
     'PointNet2Backbone': PointNet2Backbone,
     'PointNet2MSG': PointNet2MSG,
     'VoxelResBackBone8x': VoxelResBackBone8x,
+    'VoxelTransformer': VoxelTransformer,
+    'VoxelTransformerV3': VoxelTransformerV3,
 }
