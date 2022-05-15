413ae947b0dec5c7fd3562c072065b24a39d188c
Ky Anh <40734986+itskyf@users.noreply.github.com>
2022-05-15 14:33:28 +0700

    fix: server requires root to mount

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,7 @@ if [[ $PUBLIC_KEY ]]; then
 	mkdir --parents ~/.ssh && chmod a+rwx,g-rwx,o-rwx ~/.ssh
 	echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
 	chmod a+rwx,u-wx,g-rwx,o-rwx ~/.ssh/authorized_keys
-	eval "$(which sshd)"
+	service ssh start
 fi
 
 echo "Pod started"
