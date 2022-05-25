ccbf85c18ccd5e340564e78f5bcef423dc96f690
Ky Anh <40734986+itskyf@users.noreply.github.com>
2022-05-25 23:03:46 +0700

    refactor: remove easydict deps

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import subprocess
-from pathlib import Path
 from typing import List
 
 import setuptools
@@ -14,16 +12,6 @@ def make_cuda_ext(name: str, module: str, sources: List[str]):
 
 
 if __name__ == "__main__":
-    commit_short_hash = "0" * 7
-    try:
-        commit_short_hash = (
-            subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("utf-8").strip()
-        )
-    except FileNotFoundError:
-        print("Not found latest commit hash, fallback to", commit_short_hash)
-    with Path("pcdet/__init__.py").open("w", encoding="utf-8") as ver_file:
-        ver_file.write(f'__version__ = "0.5.2+{commit_short_hash}"')
-
     setuptools.setup(
         cmdclass={
             "build_ext": BuildExtension,
