fcab67032bac4957e476ffc5e4baa77ddd78efb6
Nguyễn Công Anh Khoa <nbcanhkhoa@gmail.com>
2022-06-12 05:14:10 +0700

    ...

--- pcdet/models/roi_heads/mixed_head.py
+++ pcdet/models/roi_heads/mixed_head.py
@@ -365,7 +365,7 @@ class MixedHead(RoIHeadTemplate):
 
         voxel_pooled_feature = torch.cat(pooled_features_list, dim=1)
         voxel_pooled_feature = voxel_pooled_feature.view(
-            -1, self.model_cfg.ROI_GRID_POOL.GRID_SIZE**3, voxel_pooled_feature.shape[-1]
+            -1, self.pool_cfg.GRID_SIZE**3, voxel_pooled_feature.shape[-1]
         )
 
         num_rois = batch_dict['rois'].shape[-2]
@@ -421,7 +421,7 @@ class MixedHead(RoIHeadTemplate):
             features=point_features.contiguous(),
         )
         point_pooled_features = point_pooled_features.view(
-            -1, self.model_cfg.ROI_GRID_POOL.GRID_SIZE**3, point_pooled_features.shape[-1]
+            -1, self.pool_cfg.GRID_SIZE**3, point_pooled_features.shape[-1]
         )
         pooled_features = torch.cat([voxel_pooled_feature, point_pooled_features], dim=1)
         return pooled_features, local_roi_grid_points
