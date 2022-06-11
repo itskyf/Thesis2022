9c75c239129f912f88eb0536de2b71223e263133
Nguyễn Công Anh Khoa <nbcanhkhoa@gmail.com>
2022-06-11 17:44:21 +0700

    fix init

--- pcdet/models/roi_heads/__init__.py
+++ pcdet/models/roi_heads/__init__.py
@@ -6,7 +6,7 @@ from .roi_head_template import RoIHeadTemplate
 from .second_head import SECONDHead
 from .voxelrcnn_head import VoxelRCNNHead
 from .voxelrcnn_trans_head import VoxelRCNNHeadTrans
-from .mixed_head import MixedHead
+# from .mixed_head import MixedHead
 
 __all__ = [
     "CT3DHead",
@@ -17,5 +17,5 @@ __all__ = [
     "SECONDHead",
     "VoxelRCNNHead",
     "VoxelRCNNHeadTrans",
-    "MixedHead",
+    # "MixedHead",
 ]
