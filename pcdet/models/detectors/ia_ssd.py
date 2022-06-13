2093c8be6b74c1764cebb6317f2df421475717e9
Nguyễn Công Anh Khoa <nbcanhkhoa@gmail.com>
2022-06-14 01:16:08 +0700

    ...

--- pcdet/models/detectors/ia_ssd.py
+++ pcdet/models/detectors/ia_ssd.py
@@ -15,7 +15,7 @@ class IASSD(Detector3DTemplate):
             ret_dict = {"loss": loss}
             return ret_dict, tb_dict, disp_dict
 
-        if self.model_cfg.POST_PROCESSING.get('IOU_SUPRESS', False) == False
+        if self.model_cfg.POST_PROCESSING.get('IOU_SUPRESS', False) == False:
             pred_dicts, recall_dicts = self.post_processing(batch_dict)
         else:
             pred_dicts, recall_dict = self.post_processing_iou(batch_dict)
