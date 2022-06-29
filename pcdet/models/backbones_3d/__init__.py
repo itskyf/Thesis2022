9e758e3d0d2fce088526110c594a7d1e2739785f
Nguyễn Công Anh Khoa <nbcanhkhoa@gmail.com>
2022-06-29 22:19:58 +0700

    build: pointmlp

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -1,16 +1,18 @@
 from .ia_ssd_backbone import IASSD_Backbone
 from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG
 from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x
-from .spconv_unet import UNetV2
-from .votr_backbone import VoxelTransformer, VoxelTransformerV3
+# from .spconv_unet import UNetV2
+# from .votr_backbone import VoxelTransformer, VoxelTransformerV3
+from .ia_ssd_backbone_residual import IASSD_Backbone_Residual
 
 __all__ = [
     "IASSD_Backbone",
     "PointNet2Backbone",
     "PointNet2MSG",
-    "UNetV2",
+    # "UNetV2",
     "VoxelBackBone8x",
     "VoxelResBackBone8x",
-    "VoxelTransformer",
-    "VoxelTransformerV3",
+    # "VoxelTransformer",
+    # "VoxelTransformerV3",
+    'IASSD_Backbone_Residual',
 ]
